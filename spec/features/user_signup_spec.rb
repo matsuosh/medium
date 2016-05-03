6ca0f6473e26796b6d4d41f74ffc134f9d960baf
matsuosh <shooohei.m@gmail.com>
2016-05-03 14:01:53 +0000

    Add username to sign up form

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "User signing up" do
 
   scenario "successfully" do
     visit new_user_registration_path
+    fill_in "Username", with: "Example User"
     fill_in "Email", with: "example@gmail.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
