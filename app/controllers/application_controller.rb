be33c04e09f63b17f5453943185605e9b27a5aff
matsuosh <shooohei.m@gmail.com>
2016-05-03 17:25:21 +0000

    Add links to users profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def current_user?(user)
+    current_user.id == user.id
+  end
+
+  helper_method :current_user?
+
   protected
 
     def configure_permitted_parameters
